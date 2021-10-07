964d0e93e4417edd9caf303cef9662e2ae05397d
Denisse Nova <denisse.nova.v@gmail.com>
2021-10-07 15:14:51 -0300

    if dentro del crateServer

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ const http = require('http')
 const PORT = 3000
 
 http.createServer((res, req) =>{
+  if(req.url == '/' && req.method == 'GET'){
+    res.setHeader('content-type', 'text/html');
+    res.end(fs.readFileSync('index.html', 'utf8'));
+  }
 
 }).listen(PORT, console.log(`escuchando puerto ${PORT}`))
