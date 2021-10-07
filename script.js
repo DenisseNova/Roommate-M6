f283ddf5f0fa6513398d8056b338416f9a91a20f
Denisse Nova <denisse.nova.v@gmail.com>
2021-10-07 15:02:44 -0300

    creacion servidor

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const fs = require('fs'),
+const fs = require('fs')
+const http = require('http')
+
+const PORT = 3000
+
+http.createServer((res, req) =>{
+
+}).listen(PORT, console.log(`escuchando puerto ${PORT}`))
