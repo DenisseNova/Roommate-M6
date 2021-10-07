2773aeb7012a3e7d9fa6a42c5d997dc0766fadad
Denisse Nova <denisse.nova.v@gmail.com>
2021-10-07 16:02:13 -0300

    enlazar archivo css createServer

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ const http = require('http')
 
 const PORT = 3000
 
-http.createServer((res, req) =>{
+http.createServer((req, res) =>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Max-Age', 2592000);
+
   if(req.url == '/' && req.method == 'GET'){
-    res.setHeader('content-type', 'text/html');
-    res.end(fs.readFileSync('index.html', 'utf8'));
+    res.setHeader('Content-type', 'text/html');
+    return res.end(fs.readFileSync('./index.html', 'utf8'));
   }
 
-}).listen(PORT, console.log(`escuchando puerto ${PORT}`))
+  if(req.url == '/style.css' && req.method == 'GET'){
+    return res.end(fs.readFileSync('./style.css', 'utf8'));
+  }
+  res.statusCode = 404;
+  res.end();
+}).listen(PORT, ()=> console.log(`escuchando puerto ${PORT}`))
